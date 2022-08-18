b1bfd6ad121a7b202a3b4faf30ed594a8dde9b66
Andy Cork <andy.cork@gmail.com>
2022-08-18 09:52:14 +0100

    linting

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,39 +1,39 @@
 module.exports = {
   apps: [{
-    name: 'DicomWeb-Websocket-Bridge',
-    script: '.',
-    watch: '.',
+    name: "DicomWeb-Websocket-Bridge",
+    script: ".",
+    watch: ".",
     watch_options: {
       followSymlinks: false
     },
     instances: 2,
-    log_date_format: 'YYYY-MM-DDTHH:mm:ssZ',
+    log_date_format: "YYYY-MM-DDTHH:mm:ssZ",
     wait_ready: true,
-    exec_mode: 'cluster',
+    exec_mode: "cluster",
     env_development: {
-      NODE_ENV: 'development'
+      NODE_ENV: "development"
     },
     env_production: {
-      NODE_ENV: 'production'
+      NODE_ENV: "production"
     }
   }],
 
   deploy: {
     dev: {
-      user: 'root',
-      host: 'dev-pacs.vpop-pro.com',
-      ref: 'origin/main',
-      repo: 'git@github.com-bridge:wearemothership/dicomweb-websocket-bridge.git',
-      path: '/root/dev/dicomweb-websocket-bridge',
-      'post-deploy': 'npm install && pm2 reload ecosystem.config.js --env production --update-env'
+      user: "root",
+      host: "dev-pacs.vpop-pro.com",
+      ref: "origin/main",
+      repo: "git@github.com-bridge:wearemothership/dicomweb-websocket-bridge.git",
+      path: "/root/dev/dicomweb-websocket-bridge",
+      "post-deploy": "npm install && pm2 reload ecosystem.config.js --env production --update-env"
     },
     production: {
-      user: 'root',
-      host: 'pacs.vpop-pro.com',
-      ref: 'origin/main',
-      repo: 'git@github.com-bridge:wearemothership/dicomweb-websocket-bridge.git',
-      path: '/root/dev/dicomweb-websocket-bridge',
-      'post-deploy': 'npm install && pm2 reload ecosystem.config.js --env production --update-env'
+      user: "root",
+      host: "pacs.vpop-pro.com",
+      ref: "origin/main",
+      repo: "git@github.com-bridge:wearemothership/dicomweb-websocket-bridge.git",
+      path: "/root/dev/dicomweb-websocket-bridge",
+      "post-deploy": "npm install && pm2 reload ecosystem.config.js --env production --update-env"
     }
   }
-}
+};
